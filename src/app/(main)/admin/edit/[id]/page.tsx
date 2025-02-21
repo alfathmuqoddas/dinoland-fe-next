4425af8ed3dd05109d976b7d1eec1aee421634ee
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2025-02-21 16:16:38 +0700

    update: admin edit ui

--- src/app/(main)/admin/edit/[id]/page.tsx
+++ src/app/(main)/admin/edit/[id]/page.tsx
@@ -1,21 +1,40 @@
-import { fetchWithAuth } from "@/lib/secureFetch";
+"use client";
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
 
-export default async function Edit({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-  const response = await fetchWithAuth(
-    "http://localhost:8080/api/product/" + id
-  );
-  const { name, description, price } = await response.json();
+export default function Edit() {
+  const { id } = useParams();
 
+  const [product, setProduct] = useState<any>({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/api/product/${id}`);
+        const data = await response.json();
+        if (!response.ok) {
+          throw new Error(data.error);
+        }
+        setProduct(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchProduct();
+  }, []);
+
+  const { name, description, price } = product;
   return (
     <>
-      <div>{name}</div>
-      <div>{description}</div>
-      <div>{price}</div>
+      {product ? (
+        <>
+          <div>{name}</div>
+          <div>{description}</div>
+          <div>{price}</div>
+        </>
+      ) : (
+        <>No Product exist</>
+      )}
     </>
   );
 }
